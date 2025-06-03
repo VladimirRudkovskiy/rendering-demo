35d4860016c0432e7d75bf71f37b6ff7ecfb9aa1
Vladimir Rudkovskiy <rudkovskyvladimir21051@gmail.com>
2025-06-03 14:30:53 +0300

    dynamicParams

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -1,3 +1,8 @@
+export const dyncamicParams = false;
+// true - next js will statically render pages on demand for dynamic segments not included in generateStaticParams()
+
+//false - next js will return a 404 error for any dynamic segments not included in our pre-rendered list
+
 export async function generateStaticParams() {
 	return [{ id: '1' }, { id: '2' }, { id: '3' }]; // Pre-render pages for products
 }
