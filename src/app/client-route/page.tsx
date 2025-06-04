87b3a21d06e2e15f40deda1e5d5700a97d12a7a8
Vladimir Rudkovskiy <rudkovskyvladimir21051@gmail.com>
2025-06-04 13:02:39 +0300

    Context Provider

--- src/app/client-route/page.tsx
+++ src/app/client-route/page.tsx
@@ -2,29 +2,16 @@
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
+import { useTheme } from '../components/theme-provider';
 
 
 export default function ClientRoutePage() {
+	const theme = useTheme();
 
 const settings = {
     dots: true,
   };
   return (
-    <div className="image-slider-container">
-      <Slider {...settings}>
-        <div>
-          <img src="http://picsum.photos/400/200" />
-        </div>
-        <div>
-          <img src="http://picsum.photos/400/200" />
-        </div>
-        <div>
-          <img src="http://picsum.photos/400/200" />
-        </div>
-        <div>
-          <img src="http://picsum.photos/400/200" />
-        </div>
-      </Slider>
-    </div>
+    <h1 style={{ color: theme.colors.secondary }}>Client Router Page</h1>
   );
 }
