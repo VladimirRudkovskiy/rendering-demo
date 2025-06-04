e18acb61787daf5c4f343a6a9cb8fc9471c10e8d
Vladimir Rudkovskiy <rudkovskyvladimir21051@gmail.com>
2025-06-04 13:10:47 +0300

    client only code

--- src/app/client-route/page.tsx
+++ src/app/client-route/page.tsx
@@ -1,17 +1,21 @@
 'use client';
-import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import { useTheme } from '../components/theme-provider';
+import { clientSideFunction } from '@/utils/client-utils';
 
 
 export default function ClientRoutePage() {
+	const result = clientSideFunction();
 	const theme = useTheme();
 
 const settings = {
     dots: true,
   };
   return (
-    <h1 style={{ color: theme.colors.secondary }}>Client Router Page</h1>
+		<>
+			<h1 style={{ color: theme.colors.secondary }}>Client Router Page</h1>
+			<p>{result}</p>
+		</>
   );
 }
--- src/app/server-route/page.tsx
+++ src/app/server-route/page.tsx
@@ -1,13 +1,16 @@
 import { serverSideFunction } from "@/utils/server-utils"
 import { ImageSlider } from "../components/ImageSlider";
+import { clientSideFunction } from "@/utils/client-utils";
 
 
 export default function ServerRoutePage() {
+	// const clientResult = clientSideFunction(); //will not work in server route
 
 	const result = serverSideFunction();
 	return (
 		<>
 			<h1>Server Route {result}</h1>
+			{/* <h1>{clientResult}</h1> */}
 			<ImageSlider />
 		</>
 	)
